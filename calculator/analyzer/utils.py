c77d2c7e8c426da8cee00fdfeae1fe9ed6ed38e3
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2020-04-14 12:27:15 -0400

    json function beginning

--- calculator/analyzer/utils.py
+++ calculator/analyzer/utils.py
@@ -1,6 +1,21 @@
 import os
+import json
 
 
 def create_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+def export_features_json(X, file_name):
+    data = {'numeric': {},
+            'categorical': {},
+            'checkboxes': {}}
+
+    import ipdb; ipdb.set_trace()
+
+
+
+
+    with open(file_name, 'w') as outfile:
+        json.dump(data, outfile)
--- calculator/outcome_calculator.py
+++ calculator/outcome_calculator.py
@@ -18,7 +18,7 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 from analyzer.loaders.cremona import load_cremona
 from analyzer.dataset import create_dataset
-from analyzer.utils import create_dir
+from analyzer.utils import create_dir, export_features_json
 from analyzer.learners import train_oct
 from analyzer.learners import xgboost_classifier
 from analyzer.learners import rf_classifier
@@ -37,36 +37,39 @@ X, y = create_dataset(data, prediction = prediction)
 (X_train, y_train), (X_test, y_test) = iai.split_data('classification',
                                                       X, y, train_proportion=0.8, seed=SEED)
 
+export_features_json(os.path.join(output_folder, 'test.json'))
+
+
 
 # Train trees
-output_path = os.path.join(output_folder, 'trees', folder_name)
-create_dir(output_path)
-oct_scores = train_oct(X_train, y_train, X_test, y_test, output_path, seed=SEED)
-
-
-#PARAMETERS GRID
-param_grid_XGB = {
-        "learning_rate": [0.001, 0.01, 0.1],
-        "min_samples_leaf": [4, 8, 12, 20],
-        "n_estimators": [2500, 2000, 1500, 500]}
-
-
-
-param_grid_RF = {
-        "bootstrap": [True],
-        "max_features": ['sqrt', 'log2'],
-        "min_samples_leaf": [5, 10],
-        "min_samples_split": [3, 5, 8],
-        "n_estimators": [2500, 2000, 1500, 500]}
-
-output_path_XGB = os.path.join(output_folder, 'XGB', folder_name)
-#create_dir(output_path_XGB)
-xgboost_classifier(X_train, y_train, X_test, y_test, param_grid_XGB, output_path_XGB, seed=SEED)
-
-output_path_RF = os.path.join(output_folder, 'RF', folder_name)
-#create_dir(output_path_RF)
-rf_classifier(X_train, y_train, X_test, y_test, param_grid_RF, output_path_RF, seed=SEED)
-
+#  output_path = os.path.join(output_folder, 'trees', folder_name)
+#  create_dir(output_path)
+#  oct_scores = train_oct(X_train, y_train, X_test, y_test, output_path, seed=SEED)
+#
+#
+#  #PARAMETERS GRID
+#  param_grid_XGB = {
+#          "learning_rate": [0.001, 0.01, 0.1],
+#          "min_samples_leaf": [4, 8, 12, 20],
+#          "n_estimators": [2500, 2000, 1500, 500]}
+#
+#
+#
+#  param_grid_RF = {
+#          "bootstrap": [True],
+#          "max_features": ['sqrt', 'log2'],
+#          "min_samples_leaf": [5, 10],
+#          "min_samples_split": [3, 5, 8],
+#          "n_estimators": [2500, 2000, 1500, 500]}
+#
+#  output_path_XGB = os.path.join(output_folder, 'XGB', folder_name)
+#  #create_dir(output_path_XGB)
+#  xgboost_classifier(X_train, y_train, X_test, y_test, param_grid_XGB, output_path_XGB, seed=SEED)
+#
+#  output_path_RF = os.path.join(output_folder, 'RF', folder_name)
+#  #create_dir(output_path_RF)
+#  rf_classifier(X_train, y_train, X_test, y_test, param_grid_RF, output_path_RF, seed=SEED)
+#
 
 
 
