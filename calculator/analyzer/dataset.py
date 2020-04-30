3c69b1a7eed20472de3375b47721dda858a447be
Holly Wiberg <holly.wiberg@gmail.com>
2020-04-30 13:19:11 -0400

    add feature parsing to generate explanations and bounds

--- calculator/analyzer/dataset.py
+++ calculator/analyzer/dataset.py
@@ -36,7 +36,7 @@ def create_dataset(data_dict, discharge_data = True,
     return X, y
 
 def filter_outliers(df_X, filter_lb = 0.1, filter_ub = 99.9):
-    bounds_dict = []
+    bounds_dict = {}
 
     for col in df_X:
         # lb = max(descr[col].loc['mean'] - 3*descr[col].loc['std'],0)
@@ -45,10 +45,9 @@ def filter_outliers(df_X, filter_lb = 0.1, filter_ub = 99.9):
         ub = np.ceil(np.nanpercentile(df_X[col], filter_ub))
         med = np.round(np.nanpercentile(df_X[col], 50))
 
-        bounds_dict.append({'name': col,
-            'min_val': lb,
+        bounds_dict[col] = {'min_val': lb,
             'max_val': ub,
-            'default': med})
+            'default': med}
         outlier_inds = (lb > df_X[col]) | (df_X[col] > ub)
         v = sum(outlier_inds)
         print(col+': '+ "LB = "+str(lb)+", UB = "+str(ub)+" (Filter = "+str(v)+")")
