71d2f3f95a81a2763583d1ef6cb01f88bb06becb
Holly Wiberg <holly.wiberg@gmail.com>
2020-05-12 16:32:46 -0400

    update SHAP plots with latest results and PDF format'

--- calculator/evaluation/importance.py
+++ calculator/evaluation/importance.py
@@ -3,9 +3,89 @@ import pickle
 import shap
 import matplotlib.pyplot as plt
 
-def feature_importance(model_type, model_lab, website_path, data_path, save_path):
+
+from math import sqrt 
+import matplotlib
+
+#%% Latex-style image printing
+
+SPINE_COLOR = 'gray'
+
+def latexify(fig_width=None, fig_height=None, columns=1):
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    columns : {1, 2}
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    assert(columns in [1,2])
+
+    if fig_width is None:
+        fig_width = 3.39 if columns==1 else 6.9 # width in inches
+
+    if fig_height is None:
+        golden_mean = (sqrt(5)-1.0)/2.0    # Aesthetic ratio
+        fig_height = fig_width*golden_mean # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + fig_height + 
+              "so will reduce to" + MAX_HEIGHT_INCHES + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    # NB (bart): default font-size in latex is 11. This should exactly match 
+    # the font size in the text if the figwidth is set appropriately.
+    # Note that this does not hold if you put two figures next to each other using
+    # minipage. You need to use subplots.
+    params = {'backend': 'ps',
+              'text.latex.preamble': ['\\usepackage{gensymb}'],
+              'axes.labelsize': 12, # fontsize for x and y labels (was 12 and before 10)
+              'axes.titlesize': 12,
+              'font.size': 12, # was 12 and before 10
+              'legend.fontsize': 12, # was 12 and before 10
+              'xtick.labelsize': 12,
+              'ytick.labelsize': 12,
+              'text.usetex': True,
+              'figure.figsize': [fig_width,fig_height],
+              'font.family': 'serif'
+    }
+
+    matplotlib.rcParams.update(params)
+
+
+def format_axes(ax):
+
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_color(SPINE_COLOR)
+        ax.spines[spine].set_linewidth(0.5)
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_tick_params(direction='out', color=SPINE_COLOR)
+
+    return ax
+
+#%% 
+def feature_importance(model_type, model_lab, website_path, data_path, save_path, title_mapping, 
+                       feature_limit = 100, latex = False):
     assert model_type in('mortality','infection'), "Invalid outcome"
     assert model_lab in('with_lab','without_lab'), "Invalid lab specification"
+    
+    if latex:
+        save_path = save_path+"latex_"
 
     ## Load model corresponding to *model_type* and *model_lab*.
 
@@ -27,14 +107,22 @@ def feature_importance(model_type, model_lab, website_path, data_path, save_path
     ## Calculate SHAP values (for each observation x feature)
     explainer = shap.TreeExplainer(model);
     shap_values = explainer.shap_values(X);
-
+    
+    ft_recode = []
+    for i in X.columns:
+        ft_recode.append(title_mapping[i])
+    
     ## Summarize SHAP values across all features
     # This acts as an alterative to the standard variable importance plots. Higher SHAP values translate to higher probability of mortality.
-    shap.summary_plot(shap_values, X, show=False,max_display=100)
+    plt.close()
+    latexify(fig_width = 3.4)
+    shap.summary_plot(shap_values, X, show=False,feature_names=ft_recode, max_display=feature_limit, plot_type = "violin")
     f = plt.gcf()
-    f.savefig(save_path+'summary_plot.jpg', bbox_inches='tight')
-    # f.savefig(website_path+'assets/risk_calculators/'+model_type+'/model_'+model_lab+'.jpg', bbox_inches='tight')
+    f.savefig(save_path+'summary_plot_top'+str(feature_limit)+'.pdf', bbox_inches='tight')
+    f.savefig(website_path+'assets/risk_calculators/'+model_type+'/model_'+model_lab+'.jpg', bbox_inches='tight')
     plt.clf() 
+    plt.close()
+    
  
     ## Deep-dive into individual features
     # For a given feature, see how the SHAP varies across its possible values. 
@@ -42,16 +130,21 @@ def feature_importance(model_type, model_lab, website_path, data_path, save_path
     # If omitted, it will automatically find the variable with the highest interaction.
 
     for i in X.columns:
+        plt.close()
+        latexify(fig_width = 3.4)
         shap.dependence_plot(i, shap_values, X,show=False)
         f = plt.gcf()
-        f.savefig(save_path+'dependence_plot_'+i+'.jpg', bbox_inches='tight')
+        f.savefig(save_path+'dependence_plot_'+i+'.pdf', bbox_inches='tight')
         plt.clf()
+        plt.close()
 
 
-# #%% Evaluate drivers of individual predictions 
+#%% Evaluate drivers of individual predictions 
 # # Select index j for prediction to generate.
 
-# # shap.force_plot(explainer.expected_value, shap_values[1:10,:], X.iloc[[1:10,:]])
+# shap_values = explainer.shap_values(X)
+# shap.force_plot(explainer.expected_value, shap_values, X, link = "logit")
+
 # j=20
 # plot = shap.force_plot(explainer.expected_value, shap_values[j], X.iloc[[j]] , link="logit")
 # plot
--- calculator/feature_evaluation.py
+++ calculator/feature_evaluation.py
@@ -1,3 +1,4 @@
+import os
 os.chdir('git/covid19_calculator/calculator/')
 
 #%% Load packages
@@ -6,15 +7,66 @@ import itertools
 import evaluation.descriptive as ev
 import evaluation.importance as imp
 
+#%% Load mapping (from website/risk_calculators/utils.py)
+
+title_mapping = {
+    'ABG: Oxygen Saturation (SaO2)': 'Oxygen Saturation',
+    'Alanine Aminotransferase (ALT)': 'Alanine Aminotransferase (ALT)',
+    'Age': 'Age',
+    'Aspartate Aminotransferase (AST)': 'Aspartate Aminotransferase',
+    'Blood Creatinine': 'Creatinine',
+    'Blood Sodium': 'Sodium',
+    'Blood Urea Nitrogen (BUN)': 'Blood Urea Nitrogen (BUN)',
+    'Body Temperature': 'Temperature',
+    'C-Reactive Protein (CRP)':  'C-Reactive Protein',
+    'CBC: Hemoglobin': 'Hemoglobin',
+    'CBC: Leukocytes': 'Leukocytes',
+    'CBC: Mean Corpuscular Volume (MCV)': 'Mean Corpuscular Volume',
+    'CBC: Platelets': 'Platelets',
+    'CBC: Red cell Distribution Width (RDW)': 'Red Cell Distribution Width (RDW)',
+    'Cardiac Frequency': 'Heart Rate',
+    'Cardiac dysrhythmias': 'Cardiac dysrhythmias',
+    'Gender' : 'Gender',
+    'Glycemia': 'Blood Glucose',
+    'Potassium Blood Level': 'Potassium',
+    'Prothrombin Time (INR)': 'Prothrombin Time',
+    'Systolic Blood Pressure': 'Systolic Blood Pressure (SYS)',
+    'SaO2': 'Oxygen Saturation',
+    'Blood Calcium': 'Calcium',
+    'ABG: PaO2': 'Partial Pressure Oxygen (PaO2)',
+    'ABG: pH': 'Arterial Blood Gas pH',
+    'Cholinesterase': 'Cholinesterase',
+    'Respiratory Frequency': 'Respiratory Frequency',
+    'ABG: MetHb': 'Arterial Blood Gas Methemoglobinemia',
+    'Total Bilirubin': 'Total Bilirubin',
+    'Comorbidities':'Comorbidities',
+    'Diabetes': 'Diabetes',
+    'Chronic kidney disease': 'Chronic kidney disease',
+    'Cardiac dysrhythmias': 'Cardiac dysrhythmias',
+    'Coronary atherosclerosis and other heart disease': 'Coronary atherosclerosis and other heart disease'
+}
+
 #%% Distribution by Feature
 
 ## Set paths
 website_path = '/Users/hollywiberg/git/website/'
 data_path = '/Users/hollywiberg/Dropbox (MIT)/COVID_risk/covid19_clean_data/'
 
-for model_type, model_lab in itertools.product(['mortality','infection'],['with_lab','without_lab']):
+for model_type, model_lab in itertools.product(['infection','mortality'],['with_lab','without_lab']):
+# for model_type, model_lab in itertools.product(['mortality'],['without_lab']):
     print("Model: %s, %s" %(model_type, model_lab))
     save_path = '../results/'+model_type+'/model_'+model_lab+'/'
-    imp.feature_importance(model_type, model_lab, website_path, data_path, save_path)
-    # summary = generate_summary(model_type, model_lab, title_mapping, website_path)
+    imp.feature_importance(model_type, model_lab, website_path, data_path, save_path, title_mapping, 
+                           latex = True, feature_limit = 10)
+    # summary = ev.generate_summary(model_type, model_lab, website_path, title_mapping)
     # summary.to_csv(save_path+'descriptive_statistics.csv', index = False)
+    
+# model_type = 'mortality'; model_lab = 'without_lab'
+# with open(website_path+'assets/risk_calculators/'+model_type+'/model_'+model_lab+'.pkl', 'rb') as file:
+#         model_file = pickle.load(file)
+
+
+# ft_recode = []
+# for i in X.columns:
+#     ft_recode.append(title_mapping[i])
+    
