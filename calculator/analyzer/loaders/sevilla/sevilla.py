a8c7e2422cb3d78bd1a10a08cccb7e8cb8ace261
Agni <agniorf@mit.edu>
2020-05-16 19:53:18 -0400

    adding the spanish data

--- calculator/analyzer/loaders/sevilla/sevilla.py
+++ calculator/analyzer/loaders/sevilla/sevilla.py
@@ -10,6 +10,7 @@ import pandas as pd
 import datetime
 import numpy as np
 import pickle
+import analyzer.dataset as ds
 
 
 #path = '../../../Dropbox (Personal)/COVID_clinical/covid19_sevilla'
@@ -33,34 +34,41 @@ RENAMED_COLUMNS = {
     'Current drinker (Current_drinker)':'alcohol_abuse',
     'Cancer (Any) (Cancer_Any)':'Cancer', 
     'Systolic blood pressure <90mm Hg (Systolic_blood_pressure_lt_90mm_Hg)':'Systolic Blood Pressure'}
+  
+# LABS_RENAMED_COLUMNS = {
+#     'Glucosa(mg/dL)'
     
+    
+    
+    
+#     }  
 
 
-    {'HF':'Cardiac dysrhythmias',
-    'Liver_dis':'Liver disfunction',
-    'RR_adm':'Respiratory Frequency',
-    'GLU_PRIMERA/FIRST_URG/EMERG':'Glycemia',
-    'SBP_adm':'Systolic Blood Pressure',
-    'Tbil_adm':'Total Bilirubin',
-    'WBC_adm':'CBC: Leukocytes', #needs to be divided by 1000
-    'AST_adm':'Aspartate Aminotransferase (AST)',
-    'ALT_adm':'Alanine Aminotransferase (ALT)',
-    'CRP_adm':'C-Reactive Protein (CRP)',
-    'INR_adm':'Prothrombin Time (INR)',
-    'PLT_adm':'CBC: Platelets',
-    'Creat_adm':'Blood Creatinine',
-    'DD_adm':'D-Dimer',
-    'Urea_adm':'Urea',
-    'lympho_adm':'CBC: Lymphocytes',
-    'Hb_adm':'CBC: Hemoglobin',
-    'PCT_adm':'Procalcitonin (PCT)',
-    'FERR_adm':'Ferritinin',
-    'LDH_adm':'LDH',
-    'Creat_adm':'Blood Creatinine',
-    'fibr_adm':'fibrinogen',
-    'CPK_adm':'Creatinine Kinase',
-    'UricAcid_adm':'Uric acid',
-    'trop_adm':'troponin'}
+#     {'HF':'Cardiac dysrhythmias',
+#     'Liver_dis':'Liver disfunction',
+#     'RR_adm':'Respiratory Frequency',
+#     'GLU_PRIMERA/FIRST_URG/EMERG':'Glycemia',
+#     'SBP_adm':'Systolic Blood Pressure',
+#     'Tbil_adm':'Total Bilirubin',
+#     'WBC_adm':'CBC: Leukocytes', #needs to be divided by 1000
+#     'AST_adm':'Aspartate Aminotransferase (AST)',
+#     'ALT_adm':'Alanine Aminotransferase (ALT)',
+#     'CRP_adm':'C-Reactive Protein (CRP)',
+#     'INR_adm':'Prothrombin Time (INR)',
+#     'PLT_adm':'CBC: Platelets',
+#     'Creat_adm':'Blood Creatinine',
+#     'DD_adm':'D-Dimer',
+#     'Urea_adm':'Urea',
+#     'lympho_adm':'CBC: Lymphocytes',
+#     'Hb_adm':'CBC: Hemoglobin',
+#     'PCT_adm':'Procalcitonin (PCT)',
+#     'FERR_adm':'Ferritinin',
+#     'LDH_adm':'LDH',
+#     'Creat_adm':'Blood Creatinine',
+#     'fibr_adm':'fibrinogen',
+#     'CPK_adm':'Creatinine Kinase',
+#     'UricAcid_adm':'Uric acid',
+#     'trop_adm':'troponin'}
 
 
 
@@ -116,15 +124,56 @@ LAB_METRICS = ['PATIENT ID',
                'troponin',
                'Urea']
 
+def create_dataset_v2(data_dict, discharge_data = True,
+                        comorbidities_data = True,
+                        vitals_data = True,
+                        lab_tests=True,
+                        demographics_data = False,
+                        swabs_data = False,
+                        extra_data = True,
+                        prediction='Outcome'):
 
-def load_sevilla, discharge_data = True, comorbidities_data = True, vitals_data = True, lab_tests=True, demographics_data = True, extra_data = True):
+    if demographics_data:
+        data = data_dict['demographics']
+
+    if discharge_data:
+        data = data.join(data_dict['discharge'])
+
+    if comorbidities_data:
+        data = data.join(data_dict['comorbidities'])
+
+    if vitals_data:
+        data = data.join(data_dict['vitals'])
+
+    if lab_tests:
+        data = data.join(data_dict['lab'])
+
+    if swabs_data:
+        data = data.join(data_dict['swab'])
+   
+    if extra_data:
+        data = data.join(data_dict['extras'])
+
+    X = data.loc[:, data.columns.difference(['Outcome', 'ICU', 'Swab'])]
+    y = data.loc[:, prediction]
+
+    #X = X.astype(np.float32)
+    y = y.astype(int)
+
+    return X, y
+
+
+def load_sevilla(path, discharge_data = True, comorbidities_data = True, vitals_data = True, lab_tests=False, demographics_data = True, extra_data = True):
 
     # Load admission info
     df = pd.read_csv('%s/COVID19_SASpatients_v1.csv' % path, sep=',' , encoding= 'unicode_escape')
     
     # Filter to only patients for which we know the endpoint  
     df = df[df['Discharged? (Discharged)'].notnull()]
-    df = df[~(df['Discharged? (Discharged)']=='No') & (df['Did the patient die prior to discharge? (Did_the_patient_die_prior_to_discharge)']=='No') ]
+    df = df[~(df['Discharged? (Discharged)']=='No')]
+    
+    #Drop rows for which the outcome of interest is not known
+    df = df[df['Did the patient die prior to discharge? (Did_the_patient_die_prior_to_discharge)'].notna()]
     
     #Drop columns that only contain NA values
     df = df.dropna(axis=1, how='all')
@@ -152,35 +201,30 @@ def load_sevilla, discharge_data = True, comorbidities_data = True, vitals_data 
     
     #Comorbidities
     dataset_comorbidities = df[COMORBIDITIES_COLUMNS]
-    dataset_comorbidities[COMORBIDITIES_COLUMNS] = dataset_comorbidities[COMORBIDITIES_COLUMNS].replace(['Yes','No'], [1,0])   
+    dataset_comorbidities[COMORBIDITIES_COLUMNS] = dataset_comorbidities[COMORBIDITIES_COLUMNS].replace(['Yes','No','Past'], [1,0,0])   
     
-    
-    
- 
-        
-    # Lab values
-    dataset_labs = df[LAB_METRICS]
-    
- 
-    # Oxygen saturation remove the %
-    dataset_labs['ABG: Oxygen Saturation (SaO2)'] = dataset_labs['ABG: Oxygen Saturation (SaO2)'].str.replace('%','').astype(float)
-
     #Add SaO2 in Vitals
     if not lab_tests:
-        dataset_vitals['SaO2'] = dataset_vitals['ABG: Oxygen Saturation (SaO2)']
-        
-    #Change gender decoding
-    gender = {1:0, 2:1}
-    dataset_demographics.Gender = [gender[item] for item in dataset_demographics.Gender]
+        dataset_vitals['SaO2'] = df['ABG: Oxygen Saturation (SaO2)']
+ 
+    #Change gender decoding    
+    dataset_demographics[['Gender']] = dataset_demographics[['Gender']].replace(['Female','Male'], [1,0])   
+
     
-    #Create Blood Urea Nitrogen from Urea
-    dataset_labs['Blood Urea Nitrogen (BUN)'] = dataset_labs['Urea']/2.14
-    #Drop Urea
-    dataset_labs = dataset_labs.drop(columns=['Urea'])
+    dataset_admissions[['Outcome']] = dataset_admissions[['Outcome']].replace(['Yes','No'], [1,0])   
+     
+    #Read in lab metrics
+    labs = pd.read_csv('%s/Analiticas_SASpatientesv1.csv' % path, sep=',' , encoding= 'unicode_escape')
 
-    # Leukocytes divide by 1000 to bring to the same scale
-    dataset_labs['CBC: Leukocytes'] = dataset_labs['CBC: Leukocytes']/1000
+    #Drop columns that only contain NA values
+    labs = labs.dropna(axis=1, how='all')
 
+    #Filter to only patients that are in the inclusion criteria
+    #df3 = pd.merge(dataset_admissions, labs, how='inner', left_on=['PATIENT ID','Date_Admission'], right_on=['ï»¿ID', 'FECHA'])
+    #dataset_admissions['PATIENT ID'][dataset_admissions['PATIENT ID'].isin(labs['ï»¿ID'])]
+           
+    # Lab values
+    #dataset_labs = df[LAB_METRICS]
     dataset_extra = dataset_admissions['PATIENT ID'].to_frame()
 
     for i in EXTRA_COLUMNS:
@@ -188,18 +232,19 @@ def load_sevilla, discharge_data = True, comorbidities_data = True, vitals_data 
         if not i =='PATIENT ID':
             dataset_extra[i] = np.nan
 
-    name_datasets = np.asarray(['discharge', 'comorbidities', 'vitals', 'lab', 'demographics', 'extras'])
-    dataset_array = np.asarray([discharge_data, comorbidities_data, vitals_data, lab_tests, demographics_data, extra_data])
+    name_datasets = np.asarray(['discharge', 'comorbidities', 'vitals','demographics', 'extras']) #'lab', 
+    dataset_array = np.asarray([discharge_data, comorbidities_data, vitals_data, demographics_data, extra_data])#lab_tests
 
     # Set index
     dataset_admissions.set_index('PATIENT ID', inplace=True)
     dataset_comorbidities.set_index('PATIENT ID', inplace=True)
     dataset_vitals.set_index('PATIENT ID', inplace=True)
-    dataset_labs.set_index('PATIENT ID', inplace=True)
+    #dataset_labs.set_index('PATIENT ID', inplace=True)
     dataset_demographics.set_index('PATIENT ID', inplace=True)
     dataset_extra.set_index('PATIENT ID', inplace=True)
 
     datasets = []
+
 
     # Create final dataset
     if discharge_data:
@@ -211,8 +256,8 @@ def load_sevilla, discharge_data = True, comorbidities_data = True, vitals_data 
     if vitals_data:
         datasets.append(dataset_vitals)
     
-    if lab_tests:
-        datasets.append(dataset_labs)
+    #if lab_tests:
+     #   datasets.append(dataset_labs)
 
     if demographics_data:
         datasets.append(dataset_demographics)
@@ -225,5 +270,39 @@ def load_sevilla, discharge_data = True, comorbidities_data = True, vitals_data 
     data = dict(zip(name_datasets[dataset_array], datasets))
 
     return data
+
+##############
+
+lab_tests = False
+swabs_data = False
+discharge_data = True
+comorbidities_data = True
+vitals_data = True
+demographics_data = True
+icu_data = False
+extra_data = True
+
+prediction = 'Outcome'
+
+path = '../../../Dropbox (Personal)/COVID_clinical/covid19_sevilla'
+
+data = load_sevilla(path, discharge_data = True, comorbidities_data = True, vitals_data = True, lab_tests=False, demographics_data = True, extra_data = True)
+
+X_sevilla, y_sevilla = create_dataset_v2(data,
+                                      discharge_data,
+                                      comorbidities_data,
+                                      vitals_data,
+                                      lab_tests,
+                                      demographics_data,
+                                      swabs_data,
+                                      extra_data,
+                                      prediction = prediction)
+
+sevilla = X_sevilla
+sevilla['Outcome'] = y_sevilla
+
+sevilla.to_csv(path+'/clean_data_no_labs.csv')
+
+
 
 
