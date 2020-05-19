408736d9f468a1d046db2582a6a171c8cb19799d
Luca Mingardi <luca.mingardi97@gmail.com>
2020-05-18 21:58:38 -0400

    Minor fix

--- calculator/infection_cluster.py
+++ calculator/infection_cluster.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 from sklearn.model_selection import train_test_split
-from sklearn.impute import IterativeImputer, KNNImputer
+from sklearn.impute import KNNImputer
 
 # Other packages
 from sklearn.model_selection import train_test_split, GridSearchCV
