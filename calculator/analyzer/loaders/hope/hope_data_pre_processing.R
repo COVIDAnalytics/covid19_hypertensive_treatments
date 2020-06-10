5a2278993cee57b5163696eda27152c457d23036
Holly Wiberg <holly.wiberg@gmail.com>
2020-06-10 15:31:16 -0400

    update preprocessing histograms

--- calculator/analyzer/loaders/hope/hope_data_pre_processing.R
+++ calculator/analyzer/loaders/hope/hope_data_pre_processing.R
@@ -7,13 +7,13 @@ library(reshape2)
 #source("hope_features.R")
 source("hope_data_cleaning.R")
 
-save_path = "~/Dropbox (Personal)/COVID_clinical/covid19_hope/"
-
+# save_path = "~/Dropbox (Personal)/COVID_clinical/covid19_hope/"
+save_path = "~/Dropbox (MIT)/COVID_risk/covid19_hope/"
 #In case you would like to regenerate the data uncomment this code
 #df<-create_data(save_path) 
 
 df = read.csv(paste(save_path,"hope_data_clean.csv",sep = ""), header=TRUE)
-data <- filter_columns(df)  
+data <- filter_columns (df)  
 data <-  clean_columns(data)
 str(data)
 
@@ -29,4 +29,15 @@ str(fl_data)
 #Count the extent of missing data
 na_counts = t(fl_data %>%
   select(everything()) %>% 
-  summarise_all(funs(100*sum(is.na(.))/nrow(fl_data))))
+  summarise_all(funs(100*sum(is.na(.))/nrow(fl_data)))) 
+
+na_counts
+
+data %>%
+  ggplot(aes(x = ARTERIALBLOODGAS02SATURATION)) +
+  xlim(c(0,100)) +
+  geom_histogram()
+
+data %>%
+  ggplot(aes(x = ARTERIALBLOODGASPH, group = COUNTRY)) +
+  geom_histogram()
