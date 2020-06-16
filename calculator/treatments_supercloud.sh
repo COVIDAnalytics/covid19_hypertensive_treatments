15bc36fb25e3213be9d2309b926e5c14ce3ac1fe
Agni <agniorf@mit.edu>
2020-06-16 16:43:49 -0400

    changes in bash file

--- calculator/treatments_supercloud.sh
+++ calculator/treatments_supercloud.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH -c 40
-#SBATCH --array=1:5
+#SBATCH --array=1-20
 #SBATCH --time=1-00:00
 #SBATCH --mail-user=agniorf@mit.edu
 #SBATCH --mail-type=END
 #SBATCH --output=Output.%A_%a.out
 source /etc/profile
 module load anaconda/2020a
+source "/state/partition1/llgrid/pkg/anaconda/anaconda3-2020a/etc/profile.d/conda.sh"
+unset PYTHONPATH
+export PYTHONNOUSERSITE=True
 conda activate covid19
 cd covid19_calculator/calculator/
 python treatment_cluster.py
