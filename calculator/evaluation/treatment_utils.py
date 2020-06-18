33b24e87867d5c760f3085edb4391d56dd7575f5
Holly Wiberg <holly.wiberg@gmail.com>
2020-06-18 17:52:14 -0400

    add treatment processing scripts:

---
+++ calculator/evaluation/treatment_utils.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 15:03:21 2020
+
+@author: hollywiberg
+"""
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+import analyzer.dataset as ds
+
+def load_data(data_path, split, seed = 1):
+    data = pd.read_csv(data_path)
+
+    X_hope, y_hope = ds.create_dataset_treatment(data, treatment = 'All')
+    X_hope.index.name = 'ID'
+    y_hope.index.name = 'ID'
+    Z_hope = X_hope['REGIMEN']
+    X_hope = X_hope.drop('REGIMEN', axis = 1)
+    
+    # Merge dataset
+    X = pd.concat([X_hope], join='inner', ignore_index=True)
+    Z = pd.concat([Z_hope], ignore_index=True)
+    y = pd.concat([y_hope], ignore_index=True)
+    
+    #One hot encoding
+    X = pd.get_dummies(X, prefix_sep='_', drop_first=True)
+
+    if split == 'bycountry':
+        #Split by country and then remove all columns related to country
+        train_inds = X['COUNTRY_SPAIN'] == 1
+        
+    filter_col = [col for col in X if col.startswith('COUNTRY')]
+        
+    X_train = X.loc[train_inds,].drop(filter_col, axis=1)
+    Z_train = Z[train_inds]
+    y_train = y[train_inds]
+    X_test = X.loc[-train_inds,].drop(filter_col, axis=1)
+    y_test = y[-train_inds]
+    Z_test = Z[-train_inds]
+    
+    return X_train, Z_train, y_train, X_test, Z_test, y_test
+
+def generate_preds(X, treatment, algorithm, dataset, SEED = 1, prediction = 'DEATH', split = 'bycountry'): 
+    ## Results path and file names
+    result_path = '../../covid19_treatments_results/' + str(algorithm) +'/'
+    file_list = os.listdir(result_path)
+    file_start = str(dataset)+'_results_treatment_'+str(treatment)+'_seed' + str(SEED) + '_split_' + str(split) + '_' + prediction.lower()
+    file_name = ''
+    for  f in file_list:
+        if f.startswith(file_start) & ~f.endswith('.json'):
+            file_name = result_path+f
+    
+    if file_name == '':
+        print("Invalid treatment/algorithm combination (" + str(treatment) + ", " + str(algorithm)+ ")")
+        prob_pos = np.empty(X.shape[0])
+        prob_pos[:] = np.nan
+        return prob_pos
+    else:
+        with open(file_name, 'rb') as file:
+              model_file = pickle.load(file)
+        
+        model = model_file['model']
+        
+        if model == 'json_model':
+           model_json = iai.read_json(file_name+'.json')
+        prob_pos = model.predict_proba(X)[:, 1]
+        
+        return prob_pos
+
+def algorithm_predictions(X, treatment_list, algorithm, dataset, SEED = 1, prediction = 'DEATH', split = 'bycountry'):
+    pred_list = [generate_preds(X, t, algorithm, dataset, SEED, prediction, split) for t in treatment_list]
+    df = pd.DataFrame(np.column_stack(pred_list))
+    df.columns = treatment_list
+    df.index = X.index
+    df.index.name = 'ID'
+    df['Algorithm'] = algorithm
+    df.set_index('Algorithm', append = True, inplace = True)
+    return df
--- calculator/treatment_evaluation.py
+++ calculator/treatment_evaluation.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+treatment_list': ['Chloroquine_Only', 'All', 'Chloroquine_and_Anticoagulants',
+                              'Chloroquine_and_Antivirals', 'Non-Chloroquine']#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun 17 13:57:19 2020
@@ -11,121 +12,80 @@ import os
 
 os.chdir('/Users/hollywiberg/Dropbox (MIT)/COVID_risk/covid19_calculator/calculator')
 
-import itertools
-# import evaluation_utils as u
+import evaluation.treatment_utils as u
 # import evaluation.importance as imp
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import analyzer.optuna as o
-import analyzer.dataset as ds
-import pickle
 import pandas as pd
 
 
+from julia import Julia
+jl = Julia(sysimage='/home/hwiberg/software/julia-1.2.0/lib/julia/sys_iai.so')
+from interpretableai import iai
+
 #%% Set Problem Parameters
 #Paths for data access
-jobid = 1
-
-def load_data(data_path, split, seed = 1):
-    data = pd.read_csv(data_path)
-
-    X_hope, y_hope = ds.create_dataset_treatment(data, treatment = 'All')
-    Z_hope = X_hope['REGIMEN']
-    X_hope = X_hope.drop('REGIMEN', axis = 1)
-    
-    # Merge dataset
-    X = pd.concat([X_hope], join='inner', ignore_index=True)
-    Z = pd.concat([Z_hope], ignore_index=True)
-    y = pd.concat([y_hope], ignore_index=True)
-    
-    #One hot encoding
-    X = pd.get_dummies(X, prefix_sep='_', drop_first=True)
-
-    if split == 'bycountry':
-        #Split by country and then remove all columns related to country
-        train_inds = X['COUNTRY_SPAIN'] == 1
-        
-    filter_col = [col for col in X if col.startswith('COUNTRY')]
-    regimen_col = [col for col in X if col.startswith('REGIMEN')]
-        
-    X_train = X.loc[train_inds,].drop(filter_col, axis=1)
-    Z_train = Z[train_inds]
-    y_train = y[train_inds]
-    X_test = X.loc[-train_inds,].drop(filter_col, axis=1)
-    y_test = y[-train_inds]
-    Z_test = Z[-train_inds]
-    
-    return X_train, Z_train, y_train, X_test, Z_test, y_test
         
 data_path = '../../covid19_hope/hope_matched.csv'
-X_train, Z_train, y_train, X_test, Z_test, y_test = load_data(data_path, split = 'bycountry')
+save_path = '../../covid19_treatments_results/summary/'
+
+dataset = "hope"
+treatment_list = ['Chloroquine_Only', 'All', 'Chloroquine_and_Anticoagulants',
+                              'Chloroquine_and_Antivirals', 'Non-Chloroquine']
+
+
+# algorithm_list = range(0,len(o.algo_names))
+algorithm_list = ['lr','rf','cart','xgboost']
+
 
 #%% Generate predictions
 
-jobid = 1
+split = 'bycountry'
+X_train, Z_train, y_train, X_test, Z_test, y_test = u.load_data(data_path, split = split)
 
-SEED = 1
-dataset = "hope"
-#Split type
-split_type = 0
-split_types = ["bycountry","random"]
+data_version = 'train'
 
-prediction = 'DEATH'
-treatment_list = ['Chloroquine_Only', 'All', 'Chloroquine_and_Anticoagulants',
-        'Chloroquine_and_Antivirals', 'Non-Chloroquine']
+if data_version == 'train':
+    X = X_train
+    Z = Z_train
+    y = y_train
+else: 
+    print("test")
+    X = X_test
+    Z = Z_test
+    y = y_test
 
-preds = as.d
+result = pd.concat([u.algorithm_predictions(X, algorithm = alg, dataset = dataset, treatment_list = treatment_list) 
+                    for alg in algorithm_list], axis = 0)
 
-algorithm_list = range(0,len(o.algorithms))
+# Find optimal prescription across methods
+result['Prescribe'] = result.idxmin(axis=1)
+result['Prescribe_Prediction'] = result.min(axis=1)
 
-param_list = list(itertools.product(treatment_list, algorithm_list))
+result.to_csv(save_path+data_version+'_bypatient_allmethods.csv')
 
-treatment, algorithm_id = param_list[jobid]
-algorithm = o.algorithms[algorithm_id]
-name_param = o.name_params[algorithm_id]
-name_algo = o.algo_names[algorithm_id]
+#%% Evaluate Methods
+# =============================================================================
+# Summary contains:
+# - Mean probability for each treatment (averaged across methods)
+# - Prescribe: most common prescription (mode of prescriptions across methods), list if there are ties
+# - REGIMEN: true prescribed treatment
+# - Match: indicator of whether true regimen is (in list of) optimal prescription(s)
+# =============================================================================
+summary = pd.concat([result.groupby('ID')[treatment_list].agg({'mean'}),
+          result.groupby('ID')['Prescribe'].apply(
+              lambda x:  ', '.join(pd.Series.mode(x).sort_values())),  
+          Z, y], axis=1)
+summary['Match'] = [x.replace(' ','_') in(y) for x,y in zip(summary['REGIMEN'], summary['Prescribe'])]
+summary.to_csv(save_path+data_version+'_bypatient_summary.csv')
 
-t = treatment.replace(" ", "_")
-result_path = '../../covid19_treatments_results/' + str(name_algo) +'/' + str(dataset)+'_results_treatment_'+str(t)+'_seed' + str(SEED) + '_split_' + str(split_types[split_type]) + '_' + prediction.lower() + '_jobid_' + str(jobid)
-
-def generate_preds(X, result_path): 
-    ## Results path and file names
-    # output_folder = 'predictors/treatment_mortality'
-    
-    with open(result_path, 'rb') as file:
-          model_file = pickle.load(file)
-    
-    model = model_file['model']
-    prob_pos = model.predict_proba(X)[:, 1]
-    
-    return prob_pos
-
-prob  = generate_preds(X_train, result_path)
-
-    
-    
-    
-#     model.keys()
-
-
-# # validation_paths=['../../covid19_greece/general_greek_registry.csv',
-# #                   '../../covid19_sevilla/sevilla_clean.csv']
-
-# # validation_paths={'Hellenic CSG':'../../covid19_greece/general_greek_registry.csv',
-# #                   'Sevilla':'../../covid19_sevilla/sevilla_clean.csv',
-# #                    'Hartford': '../../covid19_hartford/predictions/main'}
-
-# # #Select the model type
-# # model_types = ['mortality']
-# # model_labs = ['with_lab']
-# # seeds = list(range(30, 31))
-
-# # #Extract the seed
-
-# # SEED = [30]
-# # SPINE_COLOR = 'gray'
-# # model_type = 'mortality'
-# # threshold_list = [0.8,0.9]
-# # confidence_level = 0.95
-
-
-# # PAPER_TYPE = 'MORTALITY'  # OR PNAS
+# =============================================================================
+# Prescription_summary contains:
+# - Frequencies of prescriptions and how often they were actually prescribed
+# =============================================================================
+prescription_summary = pd.crosstab(index = summary.Prescribe, columns = summary.Match, 
+                                   margins = True, margins_name = 'Total')
+prescription_summary.columns = ['No Match', 'Match', 'Total']
+prescription_summary.drop('Total',axis=0)
+prescription_summary.sort_values('Total', ascending = False, inplace = True)
+prescription_summary.to_csv(save_path+data_version+'_bytreatment_summary.csv')
