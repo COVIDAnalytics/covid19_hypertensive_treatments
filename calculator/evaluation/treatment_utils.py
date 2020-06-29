82c6674bf6ea2981c961d252a043eec4b9d6078e
Holly Wiberg <holly.wiberg@gmail.com>
2020-06-29 00:13:06 -0400

    fix data loading

--- calculator/evaluation/treatment_utils.py
+++ calculator/evaluation/treatment_utils.py
@@ -14,46 +14,32 @@ from sklearn import metrics
 
 import analyzer.dataset as ds
 
-def load_data(data_path, split, seed = 1, file = None):
-        
-    data = pd.read_csv(data_path)
-
-    X_hope, y_hope = ds.create_dataset_treatment(data)
-    X_hope.index.name = 'ID'
-    y_hope.index.name = 'ID'
-    Z_hope = X_hope['REGIMEN']
-    X_hope = X_hope.drop('REGIMEN', axis = 1)
-    
-    # Merge dataset
-    X = pd.concat([X_hope], join='inner', ignore_index=True)
-    Z = pd.concat([Z_hope], ignore_index=True)
-    y = pd.concat([y_hope], ignore_index=True)
-    
-    #One hot encoding
+def load_data(folder, train_name, split, matched):
+    file = train_name
+    if split == 'validation':
+        file  = file.replace('_matched','')
+    elif not matched:
+        file  = file.replace('matched','unmatched')
+    file  = file.replace('train',split)
+    print(file)
+    df = pd.read_csv(folder+file)
+    X, y = ds.create_dataset_treatment(df)
+    X.index.name = 'ID'
+    y.index.name = 'ID'
+    Z = X['REGIMEN']
+    X = X.drop('REGIMEN', axis = 1)
     X = pd.get_dummies(X, prefix_sep='_', drop_first=True)
-
-    if split == 'bycountry':
-        #Split by country and then remove all columns related to country
-        train_inds = X['COUNTRY_SPAIN'] == 1
-        
-    filter_col = [col for col in X if col.startswith('COUNTRY')]
-        
-    X_train = X.loc[train_inds,].drop(filter_col, axis=1)
-    Z_train = Z[train_inds]
-    y_train = y[train_inds]
-    X_test = X.loc[-train_inds,].drop(filter_col, axis=1)
-    y_test = y[-train_inds]
-    Z_test = Z[-train_inds]
     
-    return X_train, Z_train, y_train, X_test, Z_test, y_test
+    return X, Z, y
 
-def generate_preds(X, treatment, algorithm, dataset, SEED = 1, prediction = 'DEATH', split = 'bycountry'): 
+def generate_preds(X, treatment, algorithm, matched, result_path, SEED = 1, prediction = 'DEATH'): 
     ## Results path and file names
-    result_path = '../../covid19_treatments_results/' + str(algorithm) +'/'
+    match_status =  'matched' if matched else 'unmatched'
+    result_path = result_path + str(algorithm) +'/'
     file_list = os.listdir(result_path)
-    file_start = str(dataset)+'_results_treatment_'+str(treatment)+'_seed' + str(SEED) + '_split_' + str(split) + '_' + prediction.lower()
+    file_start = str(treatment) + '_' + match_status + '_' + prediction.lower() + '_seed' + str(SEED)
     file_name = ''
-    for  f in file_list:
+    for f in file_list:
         if f.startswith(file_start) & ~f.endswith('.json'):
             file_name = result_path+f
     
@@ -65,6 +51,11 @@ def generate_preds(X, treatment, algorithm, dataset, SEED = 1, prediction = 'DEA
     else:
         with open(file_name, 'rb') as file:
              model_file = pickle.load(file)
+          
+        ## Match data to dummy variables for this dataframe
+        train = model_file['train'].drop('DEATH', axis=1)
+        X = X.reindex(labels = train.columns,  axis = 1).replace(np.nan,0)
+        
                     
         if algorithm  == 'oct':
             from julia import Julia           
@@ -78,9 +69,10 @@ def generate_preds(X, treatment, algorithm, dataset, SEED = 1, prediction = 'DEA
             prob_pos = model.predict_proba(X)[:, 1]
         
         return prob_pos
+    
 
-def algorithm_predictions(X, treatment_list, algorithm, dataset, SEED = 1, prediction = 'DEATH', split = 'bycountry'):
-    pred_list = [generate_preds(X, t, algorithm, dataset, SEED, prediction, split) for t in treatment_list]
+def algorithm_predictions(X, treatment_list, algorithm, matched, result_path, SEED = 1, prediction = 'DEATH'):
+    pred_list = [generate_preds(X, t, algorithm, matched, result_path, SEED, prediction) for t in treatment_list]
     df = pd.DataFrame(np.column_stack(pred_list))
     df.columns = treatment_list
     df.index = X.index
