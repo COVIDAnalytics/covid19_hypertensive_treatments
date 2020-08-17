6e250b5c41c3c935414b550a05954a4b4f8a086b
Agni <agniorf@mit.edu>
2020-08-17 15:52:09 +0300

    time changes

--- calculator/evaluation/treatment_timestamp.py
+++ calculator/evaluation/treatment_timestamp.py
@@ -15,6 +15,7 @@ import evaluation.treatment_utils as u
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import math
 
 #%% Set Problem Parameters - general across treatments
 
@@ -38,7 +39,7 @@ training_set_name = treatment+'_hope_hm_cremona_matched_all_treatments_train.csv
 #1. Read the origina HOPE data
 
 hope_orig = pd.read_csv('../../covid19_hope/hope_data.csv')
-    
+
 #2. Load the final results for the entire hope dataset
 hope_proc = pd.read_csv('../../covid19_treatments_data/matched_single_treatments_der_val_addl_outcomes/'+treatment+'_hope_hm_cremona_all_treatments_validation_hope.csv')
 
@@ -46,8 +47,66 @@ hope_proc = pd.read_csv('../../covid19_treatments_data/matched_single_treatments
 hope_pres = pd.read_csv(save_path+ 'validation_hope_matched_bypatient_summary_weighted.csv')
 
 #First we need to do a matching for hope_pres and hope_orig
-COUNTRY - INV_COUNTRY1
-HOSPITAL - INV_HOSPITAL1
+
+hope_proc = hope_proc.join(hope_pres.drop(['ID', 'COMORB_DEATH','REGIMEN'], axis=1))
+
+
+hope_orig['DT_HOSPITAL_ADMISSION'] = pd.to_datetime(hope_orig['DT_HOSPITAL_ADMISSION2']).dt.date
+hope_proc['DT_HOSPITAL_ADMISSION'] = pd.to_datetime(hope_proc['DT_HOSPITAL_ADMISSION']).dt.date
+hope_orig['DT_USE_CORTICOIDS'] = pd.to_datetime(hope_orig['DT_USE_CORTICOIDS2']).dt.date
+
+
+hope_orig['Date_Diff'] = (hope_orig['DT_USE_CORTICOIDS']  - hope_orig['DT_HOSPITAL_ADMISSION'])/np.timedelta64(1,'D')
+
+hope_orig.loc[hope_orig.Date_Diff < 0, 'Date_Diff'] = math.nan
+hope_orig.loc[hope_orig.Date_Diff > 30, 'Date_Diff'] = math.nan
+hope_orig.hist(column='Date_Diff')
+
+
+hope_orig[['DEATH']==1]
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+
+data = hope_orig[hope_orig['DEATH'] == 1].dropna(thresh=2).Date_Diff
+data = data[data.notnull()]
+
+plt.hist(data, weights=np.ones(len(data)) / len(data))
+
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.show()
+
+hope_orig.groupby('Date_Diff', as_index=False)['DEATH'].mean()
+
+df.groupby('StationID', as_index=False)['BiasTemp'].mean()
+
+
+data[data.notnull()]
+
+rslt_df = hope_orig[hope_orig['DEATH'] == 0] 
+rslt_df.hist(column='Date_Diff')
+
+
+.hist(column='Date_Diff')
+
+hope_orig.hist(column='Date_Diff')
+
+
+
+df = pd.merge(hope_proc, hope_orig[['DT_USE_CORTICOIDS2','DT_HOSPITAL_ADMISSION','INV_COUNTRY1','INV_HOSPITAL1','EDAD','CO_GENDER','CO_RACE','DEATH']],  how='left', left_on=['COUNTRY','HOSPITAL','AGE','GENDER','RACE','DEATH','DT_HOSPITAL_ADMISSION'], right_on = ['INV_COUNTRY1','INV_HOSPITAL1','EDAD','CO_GENDER','CO_RACE','DEATH','DT_HOSPITAL_ADMISSION'])
+
+df['DT_USE_CORTICOIDS'] = pd.to_datetime(df['DT_USE_CORTICOIDS2']).dt.date
+
+# Create a column which shows the difference in dates of admission for corticosteroids prescription
+
+df['Date_Diff'] = (df['DT_USE_CORTICOIDS']  - df['DT_HOSPITAL_ADMISSION'])/np.timedelta64(1,'D')
+
+df.hist(column='Date_Diff')
+
+#Remove duplicate rows
+#df = df.drop_duplicates(subset=['DT_HOSPITAL_ADMISSION','INV_COUNTRY1','INV_HOSPITAL1','EDAD','CO_GENDER','CO_RACE','DEATH'], keep="first")
+
+
 
 
 
